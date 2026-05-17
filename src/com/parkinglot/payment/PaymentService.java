7f0956400291e195993175b892949f61325aaefa
Rajkumar prasad <rajkumarprasadkth@gmail.com>
2026-05-17 16:49:02 +0530

    Added PaymentService to calculate parking fees based on ticket duration Implemented time-based billing using entry time and exit time difference Introduced hourly pricing strategy for different vehicle types (Bike, Car, Truck) Added duration normalization logic to ensure minimum billing for short stays Integrated VehicleType-based dynamic pricing model for flexible fee calculation

---
+++ src/com/parkinglot/payment/PaymentService.java
@@ -0,0 +1,33 @@
+package com.parkinglot.payment;
+
+import com.parkinglot.enums.VehicleType;
+import com.parkinglot.ticket.Ticket;
+
+import java.time.LocalDateTime;
+
+public class PaymentService {
+
+    private double calculateFee(Ticket ticket){
+
+        LocalDateTime existTime = LocalDateTime.now();
+
+        long hours = java.time.Duration.between(ticket.getEntryTime(), existTime).toHours();
+        if (hours == 0){
+            hours = 1;
+        }
+
+        VehicleType vehicleType = ticket.getVehicle().getVehicleType();
+        switch (vehicleType){
+
+            case BIKE :
+                return hours * 10;
+            case CAR:
+                return hours * 20;
+            case TRUCK:
+                return hours * 30;
+            default:
+                return 0;
+        }
+
+    }
+}
